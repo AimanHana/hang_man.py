cd4144d2f032f3e9a1b33fc8435fc99fa5257b67
Aiman Hana <aimanhana7@gmail.com>
2022-09-20 22:56:22 +0000

    Improving the graphics.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import sys
+import os
 lives = 6
 #Starting with a list of secret words
 words = ['monkey','camel','mouse','baboon','tiger','lion','crocodile']
@@ -7,11 +8,22 @@ words = ['monkey','camel','mouse','baboon','tiger','lion','crocodile']
 secret_word = random.choice(words)
 #display the number of letters in the secret word as underscores
 guessed_word = ['_' for x in range(len(secret_word))]
+print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""")
 print(' '.join(guessed_word))
 
-while(lives>0):
+while(lives>=0):
     guessed_letter = input("Enter a letter: ").lower()
     if guessed_letter and guessed_letter in secret_word and guessed_letter not in guessed_word:
+        print('Correct Guess, well done.')
         for index in range(len(secret_word)):
             if secret_word[index] == guessed_letter:
                 guessed_word[index] = guessed_letter
@@ -23,9 +35,19 @@ while(lives>0):
         print('You chose that letter before.')
     else:
         lives -= 1
-        print(' '.join(guessed_word))
-        print(f'Wrong letter. You have {lives} lives left.')
         if lives == 5:
+            cls = lambda: os.system('clear')
+            cls()
+            print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""",end='')
             print("""
                 -----------
                 |         |
@@ -35,8 +57,22 @@ while(lives>0):
                 |
                 |
             """)
+            print(' '.join(guessed_word))
+            print(f'Wrong letter. You have {lives} lives left.')
+           
         elif lives == 4:
-             print("""
+            cls()
+            print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""")
+            print("""
                 -----------
                 |         |
                 |         O
@@ -45,7 +81,21 @@ while(lives>0):
                 |
                 |
             """)
+            print(' '.join(guessed_word))
+            print(f'Wrong letter. You have {lives} lives left.')
+            
         elif lives == 3:
+            cls()
+            print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""")
             print("""
                 -----------
                 |         |
@@ -55,7 +105,21 @@ while(lives>0):
                 |
                 |
             """)
+            print(' '.join(guessed_word))
+            print(f'Wrong letter. You have {lives} lives left.')
+            
         elif lives == 2:
+            cls()
+            print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""")
             print("""
                 -----------
                 |         |
@@ -65,7 +129,21 @@ while(lives>0):
                 |
                 |
             """)
+            print(' '.join(guessed_word))
+            print(f'Wrong letter. You have {lives} lives left.')
+           
         elif lives == 1:
+            cls()
+            print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""")
             print("""
                 -----------
                 |         |
@@ -75,13 +153,22 @@ while(lives>0):
                 |        /
                 |
             """)
-        
+            print(' '.join(guessed_word))
+            print(f'Wrong letter. You have {lives} lives left.')
           
-    if '_' not in guessed_word:
-        print(' '.join(guessed_word))
-        print('You guessed it right. Well done.')
-        sys.exit() #quit() # exit() #This will terminate the program
-print("""
+        elif lives == 0:
+            cls()
+            print("""
+ _                                              
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|  
+                    __/ |                      
+                   |___/ 
+""")
+            print("""
                 -----------
                 |         |
                 |         O
@@ -89,6 +176,31 @@ print("""
                 |         |
                 |        / \\
                 |
-            """)           
-print('You ran out of lives. Game Over') 
+            """)
+            print(' '.join(guessed_word))
+            print(f'Wrong letter. You have {lives} lives left.')
+         
+            print("""
+                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+                ███▀▀▀██┼███▀▀▀███┼███▀█▄█▀███┼██▀▀▀
+                ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼█┼┼┼██┼██┼┼┼
+                ██┼┼┼▄▄▄┼██▄▄▄▄▄██┼██┼┼┼▀┼┼┼██┼██▀▀▀
+                ██┼┼┼┼██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██┼┼┼
+                ███▄▄▄██┼██┼┼┼┼┼██┼██┼┼┼┼┼┼┼██┼██▄▄▄
+                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼
+                ███▀▀▀███┼▀███┼┼██▀┼██▀▀▀┼██▀▀▀▀██▄┼
+                ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██┼┼┼┼██┼┼┼┼┼██┼
+                ██┼┼┼┼┼██┼┼┼██┼┼██┼┼██▀▀▀┼██▄▄▄▄▄▀▀┼
+                ██┼┼┼┼┼██┼┼┼██┼┼█▀┼┼██┼┼┼┼██┼┼┼┼┼██┼
+                ███▄▄▄███┼┼┼─▀█▀┼┼─┼██▄▄▄┼██┼┼┼┼┼██▄
+                ┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼┼            
+            """)
+            sys.exit()
+          
+    if '_' not in guessed_word:
+        # print(' '.join(guessed_word))
+        print('You guessed it right. Well done.')
+        sys.exit() #quit() # exit() #This will terminate the program
+
+
  
